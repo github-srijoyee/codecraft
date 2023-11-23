3784a589787876dfc2085f825f08dd66b46ab09e
github-srijoyee <137808302+github-srijoyee@users.noreply.github.com>
2023-11-23 13:02:05 +0530

    changes made

--- src/components/Code.jsx
+++ src/components/Code.jsx
@@ -8,12 +8,19 @@ const Container=styled(Box)`
 display:flex;
 background-color:#070764;
 height:52vh;
-
+overflow-x:auto;
+white-space: pre-wrap;
 @media only screen and (max-width: 768px){
     display:block;
-    height:auto
+    height:auto;
+    overflow:hidden;
+    
 }
 `
+const EditorContainer = styled(Box)`
+  flex: 1 1 50%; /* Adjust width of each editor box */
+  padding: 10px; /* Adjust spacing between editors */
+`;
 const Code=()=>{
     const {html,setHtml,css,setCss,js,setJs}=useContext(DataContext);
     return (
@@ -38,6 +45,7 @@ const Code=()=>{
         color="#FCD000"
         value={js}
         onChange={setJs}
+        
         />
         </Container>
     )
